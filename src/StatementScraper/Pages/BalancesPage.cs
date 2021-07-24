cd9e47d9ddca487cafeb89780538f66ebc162d24
Kane Armstrong <kane.armstrong@outlook.com>
2021-07-24 19:29:55 +1200

    Extract balance page logic to a separate class

---
+++ src/StatementScraper/Pages/BalancesPage.cs
@@ -0,0 +1,31 @@
+﻿using PuppeteerSharp;
+using StatementScraper.Extensions;
+using System;
+using System.Threading.Tasks;
+
+namespace StatementScraper.Pages
+{
+    internal class BalancesPage
+    {
+        private readonly Page _page;
+
+        public BalancesPage(Page page)
+        {
+            _page = page;
+        }
+
+        public async Task<ExportStatementPage> GoToExportStatementPage()
+        {
+            var statementsLink = await _page.GetElementOrDefault(ElementSelectors.StatementsPageLink);
+            if (statementsLink == null)
+            {
+                throw new Exception($"Failed to browse to statement export page: select '{ElementSelectors.StatementsPageLink}' was not found on the page.");
+            }
+
+            await statementsLink.ClickAsync();
+            await _page.WaitForNavigationAsync();
+
+            return new ExportStatementPage(_page);
+        }
+    }
+}
