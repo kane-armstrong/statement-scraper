3ca851f09297a5c9a6725a670a05bd156b36728c
Kane Armstrong <kane.armstrong@outlook.com>
2021-07-27 21:49:42 +1200

    wip refactor

--- src/WorkerService/TransactionEtl.cs
+++ src/WorkerService/TransactionEtl.cs
@@ -13,11 +13,11 @@ using System.Threading.Tasks;
 
 namespace WorkerService
 {
-    public class StatementSynchronizationHandler
+    public class TransactionEtl
     {
-        private readonly ILogger<StatementSynchronizationHandler> _logger;
+        private readonly ILogger<TransactionEtl> _logger;
         private readonly IBankStatementWebScraper _bankStatementWebScraper;
-        private readonly IStatementFactory _snapshotGenerator;
+        private readonly IStatementFactory _statementFactory;
         private readonly ITransactionImportJobRepository _transactionImportJobs;
         private readonly IAccountsRepository _accounts;
         private readonly ITransactionsRepository _transactions;
@@ -28,10 +28,10 @@ namespace WorkerService
         private readonly string _processedPath;
         private readonly bool _moveProcessedStatements;
 
-        public StatementSynchronizationHandler(
-            ILogger<StatementSynchronizationHandler> logger,
+        public TransactionEtl(
+            ILogger<TransactionEtl> logger,
             IBankStatementWebScraper bankStatementWebScraper,
-            IStatementFactory snapshotGenerator,
+            IStatementFactory statementFactory,
             ITransactionImportJobRepository transactionImportJobs,
             IAccountsRepository accounts,
             ITransactionsRepository transactions,
@@ -40,7 +40,7 @@ namespace WorkerService
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _bankStatementWebScraper = bankStatementWebScraper ?? throw new ArgumentNullException(nameof(bankStatementWebScraper));
-            _snapshotGenerator = snapshotGenerator ?? throw new ArgumentNullException(nameof(snapshotGenerator));
+            _statementFactory = statementFactory ?? throw new ArgumentNullException(nameof(statementFactory));
             _transactionImportJobs = transactionImportJobs ?? throw new ArgumentNullException(nameof(transactionImportJobs));
             _accounts = accounts ?? throw new ArgumentNullException(nameof(accounts));
             _transactions = transactions ?? throw new ArgumentNullException(nameof(transactions));
@@ -58,41 +58,11 @@ namespace WorkerService
             _moveProcessedStatements = options.Value.MoveProcessedStatements;
         }
 
-        public async Task DownloadBankTransactions(CancellationToken cancellationToken)
+        public async Task Run(Account account, CancellationToken cancellationToken)
         {
             await _unitOfWork.Start(cancellationToken);
 
-            CleanWorkingDirectory(_unprocessedPath);
-
-            var accounts = await _accounts.GetAccounts();
-            foreach (var account in accounts)
-            {
-                _unitOfWork.BeginTransaction();
-                var currentRun = await CreateStatementRun(account, cancellationToken);
-                _unitOfWork.Commit();
-
-                try
-                {
-                    _unitOfWork.BeginTransaction();
-                    await SynchronizeAccountTransactions(account, currentRun, cancellationToken);
-                    _unitOfWork.Commit();
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e, "Failed to load statements for account {account}: {message}", account.Identifier, e.Message);
-                    _unitOfWork.Rollback();
-
-                    _unitOfWork.BeginTransaction();
-                    currentRun.Status = "Failed: exception";
-                    await _transactionImportJobs.Save(currentRun, cancellationToken);
-                    _unitOfWork.Commit();
-                }
-            }
-        }
-
-        private static void CleanWorkingDirectory(string path)
-        {
-            var existingFiles = Directory.GetFiles(path);
+            var existingFiles = Directory.GetFiles(_options.UnprocessedStatementDirectory);
             if (existingFiles.Any())
             {
                 foreach (var existingFile in existingFiles)
@@ -100,6 +70,34 @@ namespace WorkerService
                     File.Delete(existingFile);
                 }
             }
+
+
+            _unitOfWork.BeginTransaction();
+            var job = await CreateStatementRun(account, cancellationToken);
+            _unitOfWork.Commit();
+
+            try
+            {
+                _unitOfWork.BeginTransaction();
+                var file = await DownloadStatement(account, job, cancellationToken);
+                await ExtractTransactions(account, job, file, cancellationToken);
+                _unitOfWork.Commit();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to load statements for account {account}: {message}", account.Identifier, e.Message);
+                _unitOfWork.Rollback();
+
+                _unitOfWork.BeginTransaction();
+                job.Status = "Failed: exception";
+                await _transactionImportJobs.Save(job, cancellationToken);
+                _unitOfWork.Commit();
+            }
+        }
+
+        private static void CleanWorkingDirectory(string path)
+        {
+
         }
 
         private async Task<TransactionImportJob> CreateStatementRun(Account account, CancellationToken cancellationToken)
@@ -121,37 +119,40 @@ namespace WorkerService
             return currentRun;
         }
 
-        private async Task SynchronizeAccountTransactions(Account account, TransactionImportJob currentRun, CancellationToken cancellationToken)
+        private async Task<string> DownloadStatement(Account account, TransactionImportJob job, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Loading transactions for account: {accountId} - {accountIdentifier}", account.Id, account.Identifier);
+            var downloadResult = await _bankStatementWebScraper.DownloadStatement(account, job.FromDate, job.ToDate);
+            job.Status = downloadResult.StatusMessage;
 
-            var exportedStatement = await _bankStatementWebScraper.DownloadStatement(account, currentRun.FromDate, currentRun.ToDate);
-            currentRun.Status = exportedStatement.StatusMessage;
-
-            if (!exportedStatement.Successful)
+            if (!downloadResult.Successful)
             {
                 _logger.LogWarning("Failed to download statement for {account} for the period {start} to {end}. Reason: {reason}",
-                    currentRun.AccountId, currentRun.FromDate.ToString("yyyy-MM-dd"), currentRun.ToDate.ToString("yyyy-MM-dd"),
-                    exportedStatement.StatusMessage);
-                await _transactionImportJobs.Save(currentRun, cancellationToken);
+                    job.AccountId, job.FromDate.ToString("yyyy-MM-dd"), job.ToDate.ToString("yyyy-MM-dd"),
+                    downloadResult.StatusMessage);
+                await _transactionImportJobs.Save(job, cancellationToken);
                 EnsureNoUnprocessedStatements();
-                return;
+                return null;
             }
 
-            var files = EnsureUnprocessedStatements();
+            var files = Directory.GetFiles(_options.UnprocessedStatementDirectory);
+            return files.Single();
+        }
 
-            var file = files[0];
-            var filename = Path.GetFileName(file);
+        private async Task ExtractTransactions(Account account, TransactionImportJob job, string file, CancellationToken cancellationToken)
+        {
             _logger.LogInformation("Processing file '{file}'", file);
+            var filename = Path.GetFileName(file);
 
-            var snapshot = await ReadStatement(account, file);
+            var statement = await LoadStatement(account, file);
 
-            currentRun.SourceFileName = filename;
-            currentRun.TransactionCount = snapshot.Transactions.Count;
-            account.CardOrAccountNumber = snapshot.CardOrAccountNumber;
+            job.SourceFileName = filename;
+            job.TransactionCount = statement.Transactions.Count;
+            account.CardOrAccountNumber = statement.CardOrAccountNumber;
 
-            await LoadStatementIntoDatabase(account, currentRun, snapshot, cancellationToken);
-
+            _logger.LogDebug("Processing statement. Contains {count} transactions", job.TransactionCount);
+            
+            await SaveTransactions(account, statement, cancellationToken);
+            await _transactionImportJobs.Save(job, cancellationToken);
             await _accounts.Save(account, cancellationToken);
 
             if (_moveProcessedStatements)
@@ -161,44 +162,23 @@ namespace WorkerService
                 File.Move(file, newPath);
             }
 
-            _logger.LogInformation("Finished loading transactions for account: {accountId} - {accountIdentifier}", account.Id, account.Identifier);
+            _logger.LogInformation("Finished loading transactions for account: {accountId} - {accountIdentifier}", account.Id,
+                account.Identifier);
         }
 
-        private async Task<Statement> ReadStatement(Account account, string file)
+        private async Task<Statement> LoadStatement(Account account, string file)
         {
             var bytes = await File.ReadAllBytesAsync(file);
-            var snapshot = _snapshotGenerator.Create(bytes)[account.AccountType];
+            var statement = _statementFactory.Create(bytes)[account.AccountType];
 
-            if (snapshot == null)
-                throw new Exception("Failed to deserialize the statement. Found and loaded the file, but did not recognize its content.");
-            return snapshot;
+            if (statement == null)
+                throw new Exception("Failed to load the statement. Found the file, but did not recognize its content.");
+
+            return statement;
         }
 
-        private string[] EnsureUnprocessedStatements()
+        private async Task SaveTransactions(Account account, Statement statement, CancellationToken cancellationToken)
         {
-            var files = Directory.GetFiles(_options.UnprocessedStatementDirectory);
-            if (!files.Any())
-            {
-                throw new InvalidOperationException($"No statements were found in directory '{_options.UnprocessedStatementDirectory}'");
-            }
-
-            return files;
-        }
-
-        private void EnsureNoUnprocessedStatements()
-        {
-            var unexpectedFiles = Directory.GetFiles(_options.UnprocessedStatementDirectory);
-            if (unexpectedFiles.Any())
-            {
-                _logger.LogError("Found unprocessed statements where none were expected.");
-                throw new InvalidOperationException($"Scraper reported no downloaded files, but files were found in the directory. Path: '{_options.UnprocessedStatementDirectory}'");
-            }
-        }
-
-        private async Task LoadStatementIntoDatabase(Account account, TransactionImportJob job, Statement statement, CancellationToken cancellationToken)
-        {
-            _logger.LogDebug("Processing statement. Contains {count} transactions", job.TransactionCount);
-
             foreach (var importedTransaction in statement.Transactions)
             {
                 var transaction = new Transaction
@@ -215,8 +195,16 @@ namespace WorkerService
                 };
                 await _transactions.Save(transaction, cancellationToken);
             }
-
-            await _transactionImportJobs.Save(job, cancellationToken);
+        }
+        
+        private void EnsureNoUnprocessedStatements()
+        {
+            var unexpectedFiles = Directory.GetFiles(_options.UnprocessedStatementDirectory);
+            if (unexpectedFiles.Any())
+            {
+                _logger.LogError("Found unprocessed statements where none were expected.");
+                throw new InvalidOperationException($"Scraper reported no downloaded files, but files were found in the directory. Path: '{_options.UnprocessedStatementDirectory}'");
+            }
         }
     }
 }
