6c8981aace033cacd2e0981ac2f30e55a707207e
Kane Armstrong <kane.armstrong@outlook.com>
2022-01-24 11:35:03 +1300

    Refactor to top level statements

--- src/WorkerService/Program.cs
+++ src/WorkerService/Program.cs
@@ -6,50 +6,42 @@ using StatementDeserializer;
 using StatementSaver;
 using StatementScraper;
 using System.IO;
-using System.Threading.Tasks;
+using WorkerService;
 
-namespace WorkerService;
-
-public class Program
-{
-    public static async Task Main(string[] args)
+await new HostBuilder()
+    .ConfigureAppConfiguration((_, builder) =>
     {
-        await new HostBuilder()
-            .ConfigureAppConfiguration((context, builder) =>
-            {
-                builder
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", false, true)
-                    .AddUserSecrets<Program>();
-            })
-            .ConfigureServices((context, services) =>
-            {
-                var configuration = context.Configuration;
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            .AddUserSecrets<Program>();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
 
-                Log.Logger = new LoggerConfiguration()
-                    .ReadFrom.Configuration(configuration)
-                    .Enrich.FromLogContext()
-                    .CreateLogger();
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .CreateLogger();
 
-                services.Configure<SqlConnectionFactoryOptions>(configuration.GetSection(nameof(SqlConnectionFactoryOptions)));
-                services.Configure<BankStatementWebScraperOptions>(configuration.GetSection(nameof(BankStatementWebScraperOptions)));
-                services.Configure<TransactionEtlOptions>(configuration.GetSection(nameof(TransactionEtlOptions)));
+        services.Configure<SqlConnectionFactoryOptions>(configuration.GetSection(nameof(SqlConnectionFactoryOptions)));
+        services.Configure<BankStatementWebScraperOptions>(configuration.GetSection(nameof(BankStatementWebScraperOptions)));
+        services.Configure<TransactionEtlOptions>(configuration.GetSection(nameof(TransactionEtlOptions)));
 
-                services.AddStatementDeserializer();
-                services.AddStatementSaver();
-                services.AddStatementScraper();
+        services.AddStatementDeserializer();
+        services.AddStatementSaver();
+        services.AddStatementScraper();
 
-                services.AddScoped<AccountEtl>();
-                services.AddScoped<TransactionEtl>();
-                services.AddScoped<EtlRunner>();
+        services.AddScoped<AccountEtl>();
+        services.AddScoped<TransactionEtl>();
+        services.AddScoped<EtlRunner>();
 
-                services.AddLogging(options =>
-                {
-                    options.AddSerilog(dispose: true);
-                });
+        services.AddLogging(options =>
+        {
+            options.AddSerilog(dispose: true);
+        });
 
-                services.AddHostedService<Worker>();
-            })
-            .RunConsoleAsync();
-    }
-}
+        services.AddHostedService<Worker>();
+    })
+    .RunConsoleAsync();
